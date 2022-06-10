ea53baf058249dad93a7890abcd44a385d851c36
Karlos Ye <58801593+karlosye@users.noreply.github.com>
2022-06-10 15:25:05 -0400

    Add project files.

---
+++ Pages/AcademicRecordManagement/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using lab4.DataAccess;
+
+namespace lab4.Pages.AcademicRecordManagement
+{
+    public class CreateModel : PageModel
+    {
+        private readonly lab4.DataAccess.StudentRecordContext _context;
+
+        public CreateModel(lab4.DataAccess.StudentRecordContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Student Student { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Students == null || Student == null)
+            {
+                return Page();
+            }
+
+            _context.Students.Add(Student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
